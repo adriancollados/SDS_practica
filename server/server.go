bcac23f11ae92d95565f52d5286a9fc298d0b962
Alejandro Company <alerinua@gmail.com>
2022-04-23 20:38:36 +0200

    Servidor

--- server/server.go
+++ server/server.go
@@ -2,6 +2,14 @@ package server
 
 import (
 	"net/http"
+	"time"
+	"io"
+	"encoding/json"
+	"crypto/rand"
+	"sdsGrupal/util"
+	"bytes"
+	
+	"golang.org/x/crypto/argon2"
 )
 
 func comprueba(e error) {
@@ -10,7 +18,33 @@ func comprueba(e error) {
 	}
 }
 
+type usuario struct{
+	Nombre string
+	Username string
+	Email string
+	Hash []byte
+	Sal []byte
+	Token []byte
+	Ultconexion time.Time
+}
+
+var usuarios map[string]usuario
+
+type Resp struct{
+	Correcto bool
+	Mensaje string
+	Token []byte
+}
+
+func respuesta(w io.Writer, correcto bool, mensaje string, token []byte){
+	r := Resp{Correcto: correcto, Mensaje: mensaje, Token: token}
+	rJSON, err := json.Marshal(&r)
+	comprueba(err)
+	w.Write(rJSON)
+}
+
 func Run() {
+	usuarios = make(map[string]usuario) //inicializamos el mapa de usuarios
 
 	http.HandleFunc("/", handler)
 
@@ -18,4 +52,66 @@ func Run() {
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	w.Header().Set("Content-type", "text/plain")
+
+	switch req.Form.Get("cmd") {
+	case "register":
+		_, existe := usuarios[req.Form.Get("usuario")]  //comprobamos si el usuario ya existe
+		if existe {
+			respuesta(w, false, "Usuario ya registrado", nil)
+			return
+		}
+		u := usuario{}
+		u.Nombre = req.Form.Get("usuario")
+
+		u.Sal = make([]byte, 16)
+		rand.Read(u.Sal)
+
+		contrasenya := util.Decode64(req.Form.Get("password"))
+
+		u.Hash = argon2.IDKey([]byte(contrasenya), u.Sal, 16384, 8, 1, 32)
+
+		u.Ultconexion = time.Now()
+		u.Token = make([]byte, 16)
+		rand.Read(u.Token)
+
+		usuarios[u.Nombre] = u
+		respuesta(w, true, "Usuario registrado correctamente", u.Token)
+
+	case "login":
+		u, existe := usuarios[req.Form.Get("usuario")] 
+		if !existe {
+			respuesta(w, false, "Usuario inexistente", nil)
+			return
+		}
+
+		contrasenya := util.Decode64(req.Form.Get("password"))
+		hash := argon2.IDKey([]byte(contrasenya), u.Sal, 16384, 8, 1, 32)
+		if !bytes.Equal(u.Hash, hash) {        
+			respuesta(w, false, "Credenciales inválidas", nil)
+		} else {
+			u.Ultconexion = time.Now()
+			u.Token = make([]byte, 16)
+			rand.Read(u.Token)
+			usuarios[u.Nombre] = u
+			respuesta(w, true, "Credenciales válidas", u.Token)
+		}
+
+
+	case "data":
+		u, existe := usuarios[req.Form.Get("usuario")] 
+		if !existe {
+			respuesta(w, false, "No autorizado", nil)
+			return
+		} else if (u.Token == nil) || (time.Since(u.Ultconexion).Minutes() > 60) {
+			respuesta(w, false, "No autorizado", nil)
+			return
+		} else if !bytes.EqualFold(u.Token, util.Decode64(req.Form.Get("token"))) {
+			respuesta(w, false, "No autorizado", nil)
+			return
+		}
+
+	default: respuesta(w, false, "No existe el comando", nil)
+	}	
 }
