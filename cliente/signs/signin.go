90754506b2eb906a73d51cea2994bd7e9e2d1fa0
adriancollados <adricm32@gmail.com>
2022-05-09 20:52:14 +0200

    login cliente

--- cliente/signs/signin.go
+++ cliente/signs/signin.go
@@ -1,19 +1,37 @@
 package signs
 
 import (
+	"crypto/sha512"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
-	u "sds/util"
+	"os"
+	util "sds/util"
 )
 
 func Signin(client *http.Client, cmd string) {
 	fmt.Println("Loggear un usuario")
 	fmt.Println("--------------------")
 
-	data := url.Values{} // estructura para contener los valores
-	data.Set("cmd", cmd)
+	fmt.Print("Nombre de usuario: ")
+	user := util.LeerTerminal()
+
+	fmt.Print("Contraseña: ")
+	pass := util.LeerTerminal()
+
+	// hash con SHA512 de la contraseña
+	keyClient := sha512.Sum512([]byte(pass))
+	keyLogin := keyClient[:32] // una mitad para el login (256 bits)
+	//keyData := keyClient[32:64] // la otra para los datos (256 bits)
+
+	data := url.Values{}                      // estructura para contener los valores
+	data.Set("cmd", cmd)                      // comando (string)
+	data.Set("user", user)                    // usuario (string)
+	data.Set("pass", util.Encode64(keyLogin)) // "contraseña" a base64
 	r, err := client.PostForm("https://localhost:10443", data)
-	u.Chk(err)
-	fmt.Println(r)
+	util.Chk(err)
+	io.Copy(os.Stdout, r.Body) // mostramos el cuerpo de la respuesta (es un reader)
+	r.Body.Close()             // hay que cerrar el reader del body
+	fmt.Println()
 }
